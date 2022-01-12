e526411b3927d7520cb5b1950206f232a7ab8216
cars0106 <cars0106@naver.com>
2022-01-12 13:43:00 +0900

    9th problem

---
+++ Problem9.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var a = scanner.next().toInt()
+    var b = scanner.next().toInt()
+
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+}
