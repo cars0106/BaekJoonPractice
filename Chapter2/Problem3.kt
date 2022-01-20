2a31da0059789579d35155c1ea26939650470a0d
cars0106 <cars0106@naver.com>
2022-01-20 20:17:45 +0900

    modifiy 3rd problem

--- Chapter2/Problem3.kt
+++ Chapter2/Problem3.kt
@@ -5,7 +5,7 @@ fun main() {
 
     if((year % 400) == 0)
         println("1")
-    else if(((year % 4) == 0) || ((year % 100) != 0))
+    else if(((year % 4) == 0) && ((year % 100) != 0))
         println("1")
     else
         println("0")
