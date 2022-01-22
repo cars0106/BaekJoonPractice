e5725ca785cc9deca83b23032ae0db92fe552753
cars0106 <cars0106@naver.com>
2022-01-22 20:01:46 +0900

    modifiy 4th problem in chapter3

--- Chapter3/Problem4.kt
+++ Chapter3/Problem4.kt
@@ -1,5 +1,21 @@
 package Chapter3
 
-fun main() {
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+    val n = br.readLine().toInt()
+
+    for(i: Int in 1..n) {
+        val (a, b) = br.readLine().split(" ").map { it.toInt() }
+        bw.write("${a+b}\n")
+    }
+
+    bw.flush()
+    bw.close()
+    br.close()
 }
